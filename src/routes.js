5c33c2b50e1518f552038aff35b80644c557ef6b
Leandro Carvalho <leandro7792@gmail.com>
2019-08-24 10:30:54 -0300

    testado e funcionando implantacao do sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ msg: 'oi' });
+routes.get('/', async (req, res) => {
+  const newUser = await User.create({
+    name: 'Leandro',
+    email: 'leandro@teste.com',
+    password_hash: '123456',
+  });
+
+  return res.json(newUser);
 });
 
 export default routes;
